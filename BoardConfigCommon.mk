java67/device_oneplus_sm8250-common-3

sm8250-common: Set TARGET_KERNEL_CLANG_VERSION

BoardConfigCommon.mk
@@ -56,6 +56,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r407598b
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
 # Platform

blankaf/device_oneplus_sm8250-common

BoardConfigCommon.mk
@@ -56,6 +56,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := r412851
 TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
 
 # Platform